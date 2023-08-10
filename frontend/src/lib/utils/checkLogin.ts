4c0c762c38180cf3afdbdded16aa2d9daf9202c6
hoanganh <anhnh3@bsscommerce.com>
2023-08-10 10:20:04 +0700

    update ui frontend

--- frontend/src/lib/utils/checkLogin.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "@/types/user.type";
-
-const checkLogin = (currentUser: User) =>
-  !!currentUser &&
-  currentUser?.constructor === Object &&
-  Object.keys(currentUser).length !== 0;
-
-export default checkLogin;
--- frontend/src/types/comment.type.ts
+++
@@ -1,9 +0,0 @@
-import { Article } from "./article.type";
-
-export interface Comment {
-  id: number;
-  createdAt: Date;
-  updatedAt: Date;
-  body: string;
-  article?: Article;
-}
